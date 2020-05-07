806077998fd6aac1c751194737cb52e58cee56ec
Dalto <dalton.earl@outlook.com>
2020-05-06 19:32:57 -0600

    added the save record function

--- public/db.js
+++ public/db.js
@@ -1,3 +1,36 @@
+let db
+
+const request = indexedDB.open(budget, 1)
+
+request.onupgradeneeded = function(event){
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true});
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.onLine){
+        checkDatabase();
+    }
+};
+
+request.onerror = function(event){
+    console.log(`Here is an error ${event.target.errorCode}`)
+}
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    const store = transaction.objectStore("pending");
+
+    store.add(record)
+}
+
+function checkDatabase(){
+const transaction = db.transaction(["pending", "readwrite"]);
+
+const store = transaction.objectStore("pending");
+
 const getAll = store.getALL();
 
 getAll.onsuccess = function(){
@@ -19,6 +52,6 @@ getAll.onsuccess = function(){
             store.clear();
         })
     }
+    }
 }
-
 window.addEventListener("online", checkDatabase);
